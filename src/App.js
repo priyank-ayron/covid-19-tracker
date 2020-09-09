5f87a4b218643e185a5dab86202d93e789d251a8
Priyank Agarwal <priyank-ayron@gmail.com>
2020-09-10 00:33:30 +0530

    adding the infobox component with dummy data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import Dropdown from "./Dropdown";
+import Infobox from "./Infobox";
 
 function App() {
   return (
     <>
       <Dropdown></Dropdown>
+      <div className="app_stats">
+        <Infobox title="Coronovirus Cases" total={4000} cases={32}></Infobox>
+        <Infobox title="Recovered" total={4400} cases={312}></Infobox>
+        <Infobox title="Deaths" total={3000} cases={332}></Infobox>
+      </div>
     </>
   );
 }
