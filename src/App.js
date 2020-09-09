bc4c552e24a4ae55f89d2f59f50c5c84feaa50e4
Priyank Agarwal <priyank-ayron@gmail.com>
2020-09-10 00:53:38 +0530

    app_left & app_right

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React from "react";
 import "./App.css";
 import Dropdown from "./Dropdown";
 import Infobox from "./Infobox";
+import Map from "./Map";
+import { Card, CardContent } from "@material-ui/core";
 
 function App() {
   return (
     <>
-      <Dropdown></Dropdown>
-      <div className="app_stats">
-        <Infobox title="Coronovirus Cases" total={4000} cases={32}></Infobox>
-        <Infobox title="Recovered" total={4400} cases={312}></Infobox>
-        <Infobox title="Deaths" total={3000} cases={332}></Infobox>
+      <div className="app_left">
+        <Dropdown></Dropdown>
+        <div className="app_stats">
+          <Infobox title="Coronovirus Cases" total={4000} cases={32}></Infobox>
+          <Infobox title="Recovered" total={4400} cases={312}></Infobox>
+          <Infobox title="Deaths" total={3000} cases={332}></Infobox>
+        </div>
+        <Map></Map>
       </div>
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live cases by Country</h3>
+          {/* Table */}
+          <h3>Worldwide new cases</h3>
+          {/* Graph */}
+        </CardContent>
+      </Card>
     </>
   );
 }
