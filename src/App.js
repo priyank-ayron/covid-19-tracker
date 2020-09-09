08923f6b07d7c4eaab54c8a3fabf145df825be07
Priyank Agarwal <priyank-ayron@gmail.com>
2020-09-10 01:01:57 +0530

    addig app div

--- src/App.js
+++ src/App.js
@@ -8,23 +8,29 @@ import { Card, CardContent } from "@material-ui/core";
 function App() {
   return (
     <>
-      <div className="app_left">
-        <Dropdown></Dropdown>
-        <div className="app_stats">
-          <Infobox title="Coronovirus Cases" total={4000} cases={32}></Infobox>
-          <Infobox title="Recovered" total={4400} cases={312}></Infobox>
-          <Infobox title="Deaths" total={3000} cases={332}></Infobox>
+      <div className="app">
+        <div className="app_left">
+          <Dropdown></Dropdown>
+          <div className="app_stats">
+            <Infobox
+              title="Coronovirus Cases"
+              total={4000}
+              cases={32}
+            ></Infobox>
+            <Infobox title="Recovered" total={4400} cases={312}></Infobox>
+            <Infobox title="Deaths" total={3000} cases={332}></Infobox>
+          </div>
+          <Map></Map>
         </div>
-        <Map></Map>
+        <Card className="app_right">
+          <CardContent>
+            <h3>Live cases by Country</h3>
+            {/* Table */}
+            <h3>Worldwide new cases</h3>
+            {/* Graph */}
+          </CardContent>
+        </Card>
       </div>
-      <Card className="app_right">
-        <CardContent>
-          <h3>Live cases by Country</h3>
-          {/* Table */}
-          <h3>Worldwide new cases</h3>
-          {/* Graph */}
-        </CardContent>
-      </Card>
     </>
   );
 }
