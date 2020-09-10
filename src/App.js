7a1a8108b9472217b93eecb2f0017aebc14577c5
Priyank Agarwal <priyank-ayron@gmail.com>
2020-09-10 17:36:40 +0530

    including sort function from utils file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Map from "./Map";
 import Table from "./Table";
 import { Card, CardContent } from "@material-ui/core";
 import { MenuItem, FormControl, Select } from "@material-ui/core";
+import { sortData } from "./utils";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -29,7 +30,8 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso2,
           }));
-          setTableData(data);
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
